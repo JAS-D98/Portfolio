6aa6eb7bc913216de0503692a401f24de6ecc788
Jasper Ngunjiri <jasperwambugu@gmail.com>
2023-07-18 09:04:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* scroll reveal */
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home, .heading, .contact-content', { origin: 'top' });
+ScrollReveal().reveal('.services-container, .project-container', { origin:'bottom' });
+ScrollReveal().reveal('.about-col-1, .contactInfo', { origin:'left' });
+ScrollReveal().reveal('.about-col-2, .contactForm', { origin:'right' });
