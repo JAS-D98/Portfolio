dc2c0124b02aebc145eeca13d4e68273e0453dab
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-09-19 22:16:22 +0300

    format code with prettier

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let yearText=document.querySelector("#year")
-let currentYear=new Date();
-yearText.textContent=currentYear.getFullYear();
+let yearText = document.querySelector("#year");
+let currentYear = new Date();
+yearText.textContent = currentYear.getFullYear();
 
 /* scroll reveal */
 ScrollReveal({
-    reset:true,
-    distance:'80px',
-    duration:2000,
-    delay:200
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home, .heading, .contact-content', { origin: 'top' });
-ScrollReveal().reveal('.services-container, .project-container, .mySwiper', { origin:'bottom' });
-ScrollReveal().reveal('.about-col-1, .contactInfo', { origin:'left' });
-ScrollReveal().reveal('.about-col-2, .contactForm', { origin:'right' });
+ScrollReveal().reveal(".home, .heading, .contact-content", { origin: "top" });
+ScrollReveal().reveal(".services-container, .project-container, .mySwiper", {
+  origin: "bottom",
+});
+ScrollReveal().reveal(".about-col-1, .contactInfo", { origin: "left" });
+ScrollReveal().reveal(".about-col-2, .contactForm", { origin: "right" });
